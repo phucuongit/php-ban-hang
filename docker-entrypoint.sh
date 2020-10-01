1e2359e3eea5c5a49e5b64d0f0bc437a8c2504e1
phucuongit <noname21062000@gmail.com>
2020-10-01 19:56:52 +0700

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 
 set -e;
 echo "Chown to www-data"
-chown -R www-data:www-data
+chown -R www-data:www-data ./
 echo "Create upload folder"
 mkdir -p /app/assets/img/upload
 echo "Chmod upload folder"
