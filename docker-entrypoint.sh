34d1f2dd6ad0bc6a0a9abfa5bb6d6466c6d498d9
phucuongit <noname21062000@gmail.com>
2020-10-01 19:44:47 +0700

    fdgfsd

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 
-#!/bin/sh
+#!/bin/bash
 
-# set -e;
+set -e;
 # echo "Chown to www-data"
 # chown -R www-data:www-data
 echo "Create upload folder"
-mkdir /app/assets/img/upload
+mkdir -p /app/assets/img/upload
 # echo "Chmod upload folder"
 # chmod -R 755 /app/assets/img/upload
 exec "$@"
