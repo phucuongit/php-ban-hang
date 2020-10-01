c7eaab409c8fe02dec57cd0fd962d1339f800bd3
phucuongit <noname21062000@gmail.com>
2020-10-01 20:01:37 +0700

    update odkc

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,7 @@
 #!/bin/bash
 
 set -e;
-echo "Chown to www-data"
-chown -R www-data:www-data ./
 echo "Create upload folder"
 mkdir -p /app/assets/img/upload
-chown -R www-data:www-data /app/assets/img/upload
 echo "Chmod upload folder"
-chmod -R 755 /app/assets/img/upload
+chmod -R 777 /app/assets/img/upload
