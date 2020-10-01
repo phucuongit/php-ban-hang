c3e5dd3fc876a1f9e0a5b2f82f438d5e924fa3ee
phucuongit <noname21062000@gmail.com>
2020-10-02 00:16:54 +0700

    update user

--- views/layouts/sidebarAdmin.php
+++ views/layouts/sidebarAdmin.php
@@ -12,7 +12,7 @@
                 <div class="os-content" style="padding: 0px 8px; height: 100%; width: 100%;">
                     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                         <div class="image">
-                            <img src="/assets/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image" />
+                            <img src="/assets/img/user.jpg" class="img-circle elevation-2" alt="User Image" />
                         </div>
                         <div class="info">
                             <a href="#" class="d-block"><?= $user['fullname']?></a>
